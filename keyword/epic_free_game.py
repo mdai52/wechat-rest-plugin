309184c90cb1e5743a6767d25767d37ee5989757
Yuan <49407099+1bit-cc@users.noreply.github.com>
2024-03-16 22:00:14 +0800

    Add files via upload
    add:获取EPIC免费游戏列表

---
+++ keyword/epic_free_game.py
@@ -0,0 +1,18 @@
+import requests,json
+
+api_url = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=US&allowCountries=US,CN"
+
+r = requests.get(api_url)
+if r.status_code == 200:
+    print("EPIC免费游戏列表：\n")
+    data = r.json()
+    for key in data['data']['Catalog']['searchStore']['elements']:
+        if key['promotions']:
+            if key['promotions']["promotionalOffers"] != []:
+                if key['promotions']["promotionalOffers"][0]['promotionalOffers'][0]['discountSetting']['discountPercentage'] == 0:
+                    print("{}\n{} -> {}\n".format(key["title"],key['promotions']["promotionalOffers"][0]['promotionalOffers'][0]['startDate'],key['promotions']["promotionalOffers"][0]['promotionalOffers'][0]['endDate']))
+                    # print(key["title"])
+                    # print(key['promotions']["promotionalOffers"][0]['promotionalOffers'][0]['startDate'])
+                    # print(key['promotions']["promotionalOffers"][0]['promotionalOffers'][0]['endDate'])
+else:
+    print("EPIC免费游戏列表获取失败")
